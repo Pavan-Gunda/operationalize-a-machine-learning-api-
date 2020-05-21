87adb3202bf83ba6b5638cbae9a4cfec31bc6374
ph4n666 <pavangunda66@gmail.com>
2020-05-21 00:34:13 +0000

    first commit

---
+++ upload_docker.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=pavangunda66/udacity_project4 
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+export DOCKER_ID_USER="pavangunda66"
+docker login
+docker tag pavangunda66/udacity_project4 $DOCKER_ID_USER/udacity_project4 
+
+# Step 3:
+# Push image to a docker repository
+docker push $DOCKER_ID_USER/udacity_project4 
